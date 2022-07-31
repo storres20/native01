4134c9969e93626e995667f9d34aa8d18e35b18b
storres20 <sanchotorres777@gmail.com>
2022-07-31 01:14:34 -0500

    ADD main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Main () {
+  return (
+    <View>
+      <Text>Rate Repository Application</Text>
+    </View>
+  )
+}
+
+export default Main
