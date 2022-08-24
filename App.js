2fde06a7776ef5c41862bc3a12f6359f10207028
storres20 <sanchotorres777@gmail.com>
2022-08-24 00:42:44 -0500

    FIX code

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App () {
     if (Platform.OS === 'web') {
       console.log('hello')
     } else {
-      setSelectedImage({ localUri: pickerResult })
+      setSelectedImage({ localUri: pickerResult.uri })
     }
   }
 
